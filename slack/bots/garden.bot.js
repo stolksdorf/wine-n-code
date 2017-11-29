204a40eded7569b998699231e7aadac2eb52da73
Katie <mccann.ek@gmail.com>
2017-11-28 20:09:12 -0500

    Update garden.bot.js

--- slack/bots/garden.bot.js
+++ slack/bots/garden.bot.js
@@ -1,8 +1,9 @@
 const Slack = require('pico-slack');
 
 const gardenerReady = (msg)=>{
-	if(Slack.msgHas('gardenbot')){
-		Slack.send(msg, 'Ready to garden ${msg.user}?'); 
+	if(Slack.msgHas(msg, 'gardenbot')){
+		Slack.log('This is what the msg object looks like', msg);
+		Slack.send(msg, `Ready to garden ${msg.user}?`); 
 	}
 };
 
