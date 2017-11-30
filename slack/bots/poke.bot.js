20821c90cebad3adc9f4fe37689543c62aff7666
Meg Kirkland <meg.kirkl@gmail.com>
2017-11-29 21:31:56 -0500

    Update poke.bot.js

--- slack/bots/poke.bot.js
+++ slack/bots/poke.bot.js
@@ -3,12 +3,12 @@ const Slack = require('pico-slack');
 const isWalk = (msg)=>{
 	return Slack.msgHas(msg,['walk','stroll', 'walking','strolling','jaunt','travel','skipping','run','running']);
  
-
+};
 	if(isWalk(msg)){
 	  Slack.react(msg,'beers');
     
 	};
-}
+
 
 //Slack.onMessage((msg)=>{
   
