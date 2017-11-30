dd60d796b20ba04133bbf810135a8c4e2626b4fd
rkinzie <rebecca.kinzie@shopify.com>
2017-11-29 20:48:32 -0500

    Update cat.bot.js
    updating to make this guy a cat

--- slack/bots/cat.bot.js
+++ slack/bots/cat.bot.js
@@ -1,9 +1,9 @@
 const Slack = require('pico-slack');
 
 const respond = (msg)=>{
-	if(Slack.msgHas(msg, 'wadsworth', ['hey', 'hello', 'hi', 'yo'])){
+	if(Slack.msgHas(msg, 'catbot', ['hey', 'hello', 'hi', 'yo'])){
 		Slack.log('This is what the msg object looks like', msg);
-		Slack.send(msg, `Why hello ${msg.user}`);
+		Slack.sendAs('Catbot', ':smirk_cat:', `Meow meow ${msg.user}`);
 	}
 }
 
