413dd8802c2713504f0c2abf924f421f87f67d17
rkinzie <rebecca.kinzie@shopify.com>
2017-12-13 21:05:50 -0500

    Update cat.bot.js

--- slack/bots/cat.bot.js
+++ slack/bots/cat.bot.js
@@ -1,5 +1,7 @@
 const Slack = require('pico-slack');
 const _ = require('lodash');
+const catEmojis = ['cat', 'cat2', 'smiley_cat', 'joy_cat', 'heart_eyes_cat', 'smile_cat'];
+
 
 const facts = [
 	'Did you know cats whiskers are around as wide as a cats body which helps them to sense whether a space will be wide enough for them to squeeze through? Gotta watch out for my lovely lady lumps.',
@@ -20,13 +22,10 @@ const respond = (msg)=>{
 	if(Slack.msgHas(msg, 'catbot', ['hey', 'hello', 'hi', 'yo', 'meow', 'bonjour', 'whats up'])){
 		//Slack.log('This is what the msg object looks like', msg);
 		Slack.sendAs('Catbot', ':smirk_cat:', msg, getFact());
+		Slack.react(msg, _.sample(catEmojis));
 	}
 };
 
 Slack.onMessage(respond);
 
-//Cat Fact 1: Did you know cats whiskers are around as wide as a cats body which helps them to sense whether a space will be wide enough for them to squeeze through? Gotta watch out for my lovely lady lumps.
-//Cat Fact 2: Cats conserve energy by sleeping an average of 13 to 14 hours a day. Jealous? 
-//Cat Fact 3: Cats have over 20 muscles that control their ears. So I can hear ALL the shit you talk about me.
-//Cat Fact 4: Cats can recognize the sound of your voice but just acts too cool to care. Because we are. 
-//Cat Fact 5: A house cat is faster than Usain Bolt. Wheres my medal though? 
+
